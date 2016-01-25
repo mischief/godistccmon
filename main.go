55dce48d8002f0324e1dffe09abf3bca1c6ab6a0
Nick Owens <mischief@offblast.org>
2016-01-24 18:01:59 -0800

    misc improvements
    - create distcc state dir at startup
    - scan state dir at startup
    - do no show stale jobs if child process is dead
    - show age of jobs

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"sort"
+	"syscall"
 	"time"
 
 	"github.com/nsf/termbox-go"
@@ -25,6 +27,10 @@ var states = []string{
 	"Finished",
 }
 
+func trimb(p []byte) string {
+	return string(bytes.Trim(p, "\x00"))
+}
+
 type DistccState struct {
 	StructSize int64
 	Magic      uint64
@@ -36,21 +42,41 @@ type DistccState struct {
 }
 
 func (s DistccState) String() string {
-	return fmt.Sprintf("Host: %s Slot: %d State: %s File: %s",
-		bytes.Trim(s.Host[:], "\x00"), s.Slot, states[s.State], bytes.Trim(s.File[:], "\x00"))
+	return fmt.Sprintf("Host: %s Slot: %d State: %s File: %s Child: %d",
+		trimb(s.Host[:]), s.Slot, states[s.State], trimb(s.File[:]), s.ClientPid)
 }
 
 func (s DistccState) FileStr() string {
-	return string(bytes.Trim(s.File[:], "\x00"))
+	return trimb(s.File[:])
 }
 
 func (s DistccState) HostStr() string {
-	return string(bytes.Trim(s.Host[:], "\x00"))
+	return trimb(s.Host[:])
 }
 
-type Jobs map[string]DistccState
+func (s DistccState) CheckKid() bool {
+	// dunno how this happens, but it's not valid.
+	if s.ClientPid == 0 {
+		return false
+	}
 
-type byHostSlot []DistccState
+	kerr := syscall.Kill(int(s.ClientPid), syscall.Signal(0))
+	if kerr == nil || kerr == syscall.EPERM {
+		return true
+	}
+
+	return false
+}
+
+type MonitorState struct {
+	DistccState
+
+	StartTime time.Time
+}
+
+type Jobs map[string]MonitorState
+
+type byHostSlot []MonitorState
 
 func (s byHostSlot) Len() int      { return len(s) }
 func (s byHostSlot) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
@@ -63,8 +89,8 @@ func (s byHostSlot) Less(i, j int) bool {
 	return s1 < s2
 }
 
-func (j Jobs) Sort() []DistccState {
-	s := make([]DistccState, 0)
+func (j Jobs) Sort() []MonitorState {
+	s := make([]MonitorState, 0)
 
 	for _, st := range j {
 		s = append(s, st)
@@ -75,6 +101,55 @@ func (j Jobs) Sort() []DistccState {
 	return s
 }
 
+func (j Jobs) Load(path string) error {
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	br := bytes.NewReader(buf)
+
+	ds := DistccState{}
+
+	err = binary.Read(br, binary.LittleEndian, &ds)
+	if err != nil {
+		return err
+	}
+
+	file := bytes.Trim(ds.File[:], "\x00")
+	host := bytes.Trim(ds.Host[:], "\x00")
+
+	copy(ds.File[:], file)
+	copy(ds.Host[:], host)
+
+	if len(file) == 0 || len(host) == 0 {
+		return err
+	}
+
+	if !ds.CheckKid() {
+		return fmt.Errorf("child %d is dead", ds.ClientPid)
+	}
+
+	if _, ok := j[path]; ok {
+		// update
+		ms := j[path]
+		delete(j, path)
+		ms.DistccState = ds
+		j[path] = ms
+		return nil
+	}
+
+	j[path] = MonitorState{
+		DistccState: ds,
+		StartTime:   time.Now().Round(1 * time.Second),
+	}
+	return nil
+}
+
+func (j Jobs) Remove(name string) {
+	delete(j, name)
+}
+
 func str(x, y int, s string) {
 	for _, c := range s {
 		termbox.SetCell(x, y, c, termbox.ColorGreen, termbox.ColorDefault)
@@ -82,6 +157,23 @@ func str(x, y int, s string) {
 	}
 }
 
+func draw(jobs Jobs) {
+	t := time.Now()
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+
+	j := jobs.Sort()
+
+	str(1, 0, fmt.Sprintf("%d active jobs.", len(j)))
+	str(0, 1, fmt.Sprintf("%-15s %-8s %-15s %-25s %-8s %-20s", "Host", "Slot", "State", "File", "Age"))
+
+	i := 2
+	for _, st := range j {
+		str(0, i, fmt.Sprintf("%-15s %-8d %-15s %-25s %-20s", st.HostStr(), st.Slot, states[st.State], st.FileStr(), t.Round(1*time.Second).Sub(st.StartTime)))
+		i++
+	}
+
+	termbox.Flush()
+}
 func main() {
 	distcc := os.Getenv("DISTCC_DIR")
 	if len(distcc) == 0 {
@@ -89,6 +181,9 @@ func main() {
 	}
 
 	distcc += "/state"
+
+	// try to make it if it doesn't exist
+	os.MkdirAll(distcc, 0775)
 
 	watcher, err := inotify.NewWatcher()
 	if err != nil {
@@ -98,6 +193,15 @@ func main() {
 	err = watcher.Watch(distcc)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	jobs := Jobs{}
+
+	// scan dir at startup
+	files, _ := ioutil.ReadDir(distcc)
+	for _, f := range files {
+		fp := filepath.Join(distcc, f.Name())
+		jobs.Load(fp)
 	}
 
 	event := make(chan termbox.Event)
@@ -117,30 +221,18 @@ func main() {
 
 	timer := time.Tick(500 * time.Millisecond)
 
-	jobs := Jobs{}
-
-	draw := func() {
-		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-
-		j := jobs.Sort()
-
-		str(1, 0, fmt.Sprintf("%d active jobs.", len(j)))
-		str(0, 1, fmt.Sprintf("%-15s %-8s %-15s %-15s", "Host", "Slot", "State", "File"))
-
-		i := 2
-		for _, st := range j {
-			str(0, i, fmt.Sprintf("%-15s %-8d %-15s %-15s", st.HostStr(), st.Slot, states[st.State], st.FileStr()))
-			i++
-		}
-
-		termbox.Flush()
-	}
-
 mainloop:
 	for {
 		select {
 		case <-timer:
-			draw()
+			// check kids
+			for n, j := range jobs {
+				if !j.CheckKid() {
+					delete(jobs, n)
+				}
+			}
+
+			draw(jobs)
 		case ev := <-event:
 			switch ev.Type {
 			case termbox.EventKey:
@@ -158,32 +250,11 @@ mainloop:
 			}
 		case ev := <-watcher.Event:
 			if ev.Mask&inotify.IN_MODIFY > 0 {
-				buf, _ := ioutil.ReadFile(ev.Name)
-
-				br := bytes.NewReader(buf)
-
-				ds := DistccState{}
-
-				err := binary.Read(br, binary.LittleEndian, &ds)
-				if err != nil {
-					break
-				}
-
-				file := bytes.Trim(ds.File[:], "\x00")
-				host := bytes.Trim(ds.Host[:], "\x00")
-
-				copy(ds.File[:], file)
-				copy(ds.Host[:], host)
-
-				if len(file) == 0 || len(host) == 0 {
-					break
-				}
-
-				jobs[ev.Name] = ds
+				jobs.Load(ev.Name)
 			}
 
 			if ev.Mask&inotify.IN_DELETE > 0 {
-				delete(jobs, ev.Name)
+				jobs.Remove(ev.Name)
 			}
 
 		case err := <-watcher.Error:
